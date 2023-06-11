bcc412004897fe6231aa8fea5107edb839864fc0
Jordy <jbonn2002@gmail.com>
2023-06-10 20:32:55 -0700

    added songs to feed, added middleware, and started song list

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["vwczkdbqjnonsgjrasuu.supabase.co"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
